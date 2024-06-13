9069c80b4bfaed0025a21bb5d4b527a294089b7c
Wilfredo <xvwilito11@gmail.com>
2024-06-13 09:53:35 -0600

    ejemplo

--- src/navigator/TopTabNavigator.js
+++ src/navigator/TopTabNavigator.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import StackNavigator from './StackNavigator';
-import SettingsScreen from '../screens/SettingsScreen';
 import Screen1 from '../screens/Screen1';
 import Screen2 from '../screens/Screen2';
 
